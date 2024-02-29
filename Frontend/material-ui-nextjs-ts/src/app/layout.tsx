872bd22a48e6c281e10b61de037660704108547d
Joshua Classen <info@joshua-classen.de>
2024-02-29 14:08:30 +0100

    Added Frontend Dashboard

--- Frontend/material-ui-nextjs-ts/src/app/layout.tsx
+++ Frontend/material-ui-nextjs-ts/src/app/layout.tsx
@@ -1,8 +1,11 @@
-import * as React from 'react';
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import theme from '@/theme';
+"use client";
+
+import * as React from "react";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import theme from "@/theme";
+import Dashboard from "@/components/dashboard/Dashboard";
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
@@ -11,8 +14,9 @@ export default function RootLayout(props: { children: React.ReactNode }) {
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
           <ThemeProvider theme={theme}>
             {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-            <CssBaseline />
-            {props.children}
+            {/* <CssBaseline />
+            {props.children} */}
+            <Dashboard></Dashboard>
           </ThemeProvider>
         </AppRouterCacheProvider>
       </body>
